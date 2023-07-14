2f43cdcec34883f3b9e9d353861a18abb11908ea
SE-AP <apiafisebastine@gmail.com>
2023-07-13 19:34:04 -0700

    C program that prints using printf function

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 /**
- * main - Program that prints using printf
+ * main -A Program that prints using printf function
  * Return: 0 (Perfect)
  */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n")
-		return (0);
+printf("with proper grammar,but the outcome is a piece of art,\n");
+return (0);
 }
 
 
