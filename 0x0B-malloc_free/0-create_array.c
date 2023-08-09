fa0d2eb45ce33a9e33c3109a9b750aacb197c039
SE-AP <apiafisebastine@gmail.com>
2023-08-08 18:48:00 -0700

    update

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -25,24 +25,3 @@ char *create_array(unsigned int size, char c)
 
 	return str;
 }
-
-int main() {
-	unsigned int array_size = 10;
-	char fill_character = 'X';
-
-	char *result_array = create_array(array_size, fill_character);
-
-	if (result_array == NULL) {
-		printf("Memory allocation failed.\n");
-	return 1; 
-	}
-
-	for (unsigned int i = 0; i < array_size; i++) {
-		printf("%c ", result_array[i]);
-    }
-	printf("\n");
-
-	free(result_array);
-   
-	return 0;
-}
